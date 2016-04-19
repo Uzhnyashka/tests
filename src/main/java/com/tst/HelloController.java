14cddd61828bf17651610cbf393d90aa3d29628c
EgorBobyk <egor4k.964@gmail.com>
2016-04-19 15:27:18 +0300

    Initial commit

---
+++ src/main/java/com/tst/HelloController.java
@@ -0,0 +1,28 @@
+package com.tst;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by bobyk on 19/04/16.
+ */
+
+@Controller
+public class HelloController extends AbstractController {
+
+    @Override
+    @RequestMapping(value = ("/welcome"))
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+
+        ModelAndView model = new ModelAndView("HelloWorld");
+        model.addObject("msg", "hello world");
+
+        return model;
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/HelloWorld.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bobyk
+  Date: 19/04/16
+  Time: 13:04
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Privet</title>
+</head>
+<body>
+    <h1>Spring MVC Hello World Expamle</h1>
+    <h2>$(msg)</h2>
+</body>
+</html>
